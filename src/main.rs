1a01d18b3dff0ad6e74f2544fbdb5823ccff2e30
Ashish Singh <ashishsingh21798@gmail.com>
2023-04-13 20:42:31 +0530

    inital rust code

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Print to console
+    println!("Hello, world!");
+
+    // Basic Formating
+    println!("{} divided by {} equal to {}",10, 2, 5);
+
+    // Positional Arguments
+    println!(
+        "{0} is form {1} and {0} likes to {2}", 
+        "Brad", "Mass","code"
+    );
+}
